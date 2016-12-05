d6e0eb8de5df86c182ab3a33201fb8b010a9f087
justin holland <jjholland914@gmail.com>
2016-12-05 18:08:13 -0500

    adding search page

---
+++ Phase5/Phase5/Search.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="Phase5.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ Phase5/Phase5/Search.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Phase5
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Data Source=db.webteach.iu.edu;User ID=zhao20_1583911;Password=7iCXpYl"].ConnectionString);
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //connectionstring
+            string search = "select * from LicenseDB where(Business_Name like '%' + @business_Name + '%')";// or Phone like '&' + @phone + '%')";
+            SqlCommand comm = new SqlCommand(search, con);
+            comm.Parameters.Add("@business_Name", SqlDbType.NVarChar).Value = TextBox1.Text;
+
+
+            con.Open();
+            comm.ExecuteNonQuery();
+
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = comm;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Business_Name");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+
+            con.Close();
+
+
+        }
+
+    }
